322c22fa39724d5418ab65717d11975baa7449c9
Chenda Zhang <chenda1997@hotmail.com>
2020-10-08 11:49:17 +0200

    Support for command line, updated readme file

--- README.md
+++ README.md
@@ -1,10 +1,20 @@
 # ReplaceTopicTerms
 
-Create a ReplaceTopicTerms object with argument of a gavagai export excel report\
-translator=ReplaceTopicTerms(gavagai_excel_filename:String)\
-Add more mappings by providing extra excel reports\
-translator.load_mapping_gavagai(filename)\
-Translate one sentence:\
-translator.replace_one_review(sentence:String)\
-Translate a Gavagai explorer input csv file:\
-translator.update_gavagai_input_file(filename:String)
+This project provides tool for word replacement for a Gavagai import csv file
+using a map table generated with a Gavagai Explorer output filename
+
+##How to use
+e.g. `python3 ReplaceTopicTerms.py -m gavagai_export_file.xlsx -f file_tobe_updated.csv -c Review -l english`
+
+##Arguments
+### -f --filename
+filepath of the csv file to be updated
+### -m --mappingfile
+filepath to the gavagai export xlsx file that is used for generating
+the mapping table
+### -l --language
+Language of the stop words
+### -o -outdir
+Directory path for the result file/plots to be stored
+### -c --column
+Target Column to be analyzed(Default:Review)
--- ReplaceTopicTerms.py
+++ ReplaceTopicTerms.py
@@ -5,6 +5,8 @@ import os
 from sklearn.feature_extraction.text import TfidfVectorizer,CountVectorizer
 import numpy as np
 from matplotlib import pyplot as plt
+import argparse
+
 
 
 class ReplaceTopicTerms:
@@ -48,7 +50,7 @@ class ReplaceTopicTerms:
                     return self.replace_one_review(sentence.lower().split(key,1)[0])+mapping_table[key]+self.replace_one_review(sentence.lower().split(key,1)[1])
         return sentence
 
-    def update_gavagai_input_file(self,gavagai_input_file,target_column="Review",output_name=None,generate_report=True):
+    def update_gavagai_input_file(self,gavagai_input_file,target_column="Review",language='english',output_name=None,generate_report=True,outpath='Result/'):
         '''Update all the reviews/sentences in the default Gavagai explorer input csv file
             Input: String| file path to the input file
             Output:Bool |True if a new file is generated, False otherwise
@@ -69,7 +71,7 @@ class ReplaceTopicTerms:
                     f_write.close()
                 f.close()
                 if generate_report:
-                    self.generate_report(gavagai_input_file,output_name,target_column)
+                    self.generate_report(gavagai_input_file,output_name,target_column,language=language,outdir=outpath)
                 return True
         except:
             print("Unexpected error:", sys.exc_info())
@@ -88,19 +90,20 @@ class ReplaceTopicTerms:
                     reverse_map[map[key]]=[key]
         return reverse_map
 
-    def generate_report(self,original_file,updated_file,target_column):
+    def generate_report(self,original_file,updated_file,target_column,outdir='Result/',language='english'):
         '''Generate report that compares the original file and the updated file, using words in the existed mapping table
             Input:Strings|
             Output:Bool|'''
-        if not os.path.exists('Result'):
-            os.makedirs('Result')
+        if outdir=='Result':
+            if not os.path.exists('Result'):
+                os.makedirs('Result')
         filelist=[original_file,updated_file]
-        self.export_frequency(filelist,target_column,outpath='Result/',legends=["Original","Updated"])
+        self.export_frequency(filelist,target_column,outpath=outdir,legends=["Original","Updated"],language=language)
 
         reverse_map=self.reverse_mapping()
-        tfidf_original = self.compute_tfidf(original_file, target_column)
+        tfidf_original = self.compute_tfidf(original_file, target_column,language=language)
         #tfidf_original.replace(0,np.NaN)
-        tfidf_updated = self.compute_tfidf(updated_file, target_column)
+        tfidf_updated = self.compute_tfidf(updated_file, target_column,language=language)
         #tfidf_updated.replace(0,np.NaN)
         for key in reverse_map:
             key_array=tfidf_original[key]
@@ -113,7 +116,7 @@ class ReplaceTopicTerms:
 
 
 
-            with open('Result/tfidf_summary_'+original_file[0:-4]+'.txt','a') as f:
+            with open(outdir+'tfidf_summary_'+original_file[0:-4]+'.txt','a') as f:
                 f.write("After Update:"+key+str(topic_value))
                 f.write('\n')
                 f.write(",".join(original_value))
@@ -135,7 +138,7 @@ class ReplaceTopicTerms:
 
 
 
-    def compute_tfidf(self,filename,target_column,voc=None):
+    def compute_tfidf(self,filename,target_column,voc=None,language='english'):
         '''Internal Use, compute tf-idf matrix
         filename:String|file path to the input file
         target_column:String|The column name of the texts that is going to be analyzed'''
@@ -143,27 +146,26 @@ class ReplaceTopicTerms:
             voc=self.vocabulary
         df1=pd.read_csv(filename)
         corpus=df1[target_column]
-        vectorizer=TfidfVectorizer(analyzer='word',stop_words='english')
+        vectorizer=TfidfVectorizer(analyzer='word',stop_words=language)
         X=vectorizer.fit_transform(corpus)
         full_vocab=vectorizer.get_feature_names()
         full_vocab=self.add_vacab(full_vocab)
-        vectorizer = TfidfVectorizer(analyzer='word', stop_words='english',vocabulary=full_vocab)
+        vectorizer = TfidfVectorizer(analyzer='word', stop_words=language,vocabulary=full_vocab)
         X = vectorizer.fit_transform(corpus)
         tfidf_tokens=vectorizer.get_feature_names()
-        print(tfidf_tokens)
         df_tfidf=pd.DataFrame(data=X.toarray(),columns=tfidf_tokens)
-        print(df_tfidf)
+
 
         return df_tfidf
 
-    def compute_frequency(self,filename,target_column):
+    def compute_frequency(self,filename,target_column,language='english'):
         '''Internal Use, compute word frequency
         filename:String|file path to the input file
         target_column:String|The column name of the texts that is going to be analyzed
         returns a pandas dataframe of the count matrix'''
         df1 = pd.read_csv(filename)
         corpus = df1['Review']
-        countVectorizer = CountVectorizer(analyzer='word', stop_words='english')
+        countVectorizer = CountVectorizer(analyzer='word', stop_words=language)
         X2 = countVectorizer.fit_transform(corpus)
         count_tokens = countVectorizer.get_feature_names()
         df_count = pd.DataFrame(data=X2.toarray(), columns=count_tokens)
@@ -172,7 +174,7 @@ class ReplaceTopicTerms:
         column.columns=['count']
         return column.sort_values(by="count")
 
-    def export_frequency(self,filelist,target_column,outpath=None,bin_intervals=None,legends=None):
+    def export_frequency(self,filelist,target_column,outpath=None,bin_intervals=None,legends=None,language='english'):
         '''Generate export files that compares the word frequency between the original csv file and updated one
             filelist:list(Strings)|iterable of path of filenames that needed to be ploted
             target_column:String|The column name of the texts that is going to be analyzed
@@ -184,7 +186,7 @@ class ReplaceTopicTerms:
         #sum_columns=[]
         np_sums=[]
         for filename in filelist:
-            df=self.compute_frequency(filename,target_column)
+            df=self.compute_frequency(filename,target_column,language=language)
             #sum_columns.append(self.sort_column(df.sum(axis=0).to_frame()))
             np_sums.append(df.sum(axis=0).to_numpy())
             sum_column=self.sort_column(df.sum(axis=0).to_frame())
@@ -215,8 +217,24 @@ class ReplaceTopicTerms:
             plt.savefig(outpath+"frequency_plot_from {} to {}.png".format(np.min(bin_interval),np.max(bin_interval)),dpi=240)
 
 def main():
-    translator=ReplaceTopicTerms("example_hotel.xlsx")
-    translator.update_gavagai_input_file("example_hotel.csv")
+    parser=argparse.ArgumentParser(description="Update gavagai input files by replacing topic terms to topic representation")
+    parser.add_argument('-c','--column',type=str,default='Review',help="Target Column to be analyzed(Default:Review)")
+    parser.add_argument('-l','--language',type=str,default='english',help='Language of the text(Default:english)')
+    parser.add_argument('-m','--mappingfile',type=str,help="File path to the gavagai output file that is used to generate the mapping table,required",required=True)
+    parser.add_argument('-f','--filename',type=str,help='File path to the csv file that is going to be updated',required=True)
+    parser.add_argument('-o','--outdir',type=str,help='Directory path for the export result files',required=False)
+    args=parser.parse_args()
+
+
+    translator=ReplaceTopicTerms(args.mappingfile)
+    if args.outdir:
+        args.outdir = args.outdir + '/'
+        print(args)
+        translator.update_gavagai_input_file(args.filename,args.column,language=args.language,outpath=args.outdir)
+    else:
+        translator.update_gavagai_input_file(args.filename, args.column, language=args.language)
+
+
 
 
 
